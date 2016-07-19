07289fe138eb31d8d1db02fea6232c3a94c36f25
Bhuvnesh Chaudhary <bhuvneshchaudhary@bhuviMac.local>
2016-07-18 23:32:51 -0700

    updated

--- ambari-ci/jobs/build/scripts/create-tarball.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp phd-misc-peacock/stack-utils/setup_repo.sh build-ambari-rpms/ &&
-chown -R root:root build-ambari-rpms &&
-createrepo build-ambari-rpms/ &&
-tar cvzf AMBARI-OUTPUT.tar.gz build-ambari-rpms &&
