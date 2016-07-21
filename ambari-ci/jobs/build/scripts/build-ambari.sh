1a8ab44a2d0edf0535cbf6f2b891262526253b1b
Bhuvnesh Chaudhary <bhuvneshchaudhary@bhuviMac.local>
2016-07-21 01:26:07 -0700

    updated

--- ambari-ci/jobs/build/scripts/build-ambari.sh
+++ ambari-ci/jobs/build/scripts/build-ambari.sh
@@ -17,20 +17,20 @@ popd &&
 
 # Copy the generated rpms to target output folder
 mkdir -p ambari_artifacts
-OUPUT_FOLDER=ambari_artifacts/${AMBARI_RPM_FOLDER}
-mkdir -p ${OUPUT_FOLDER} &&
-cp ambari/ambari-server/target/rpm/ambari-server/RPMS/x86_64/ambari-server-*.rpm ${OUPUT_FOLDER}/ &&
+OUTPUT_FOLDER=ambari_artifacts/${AMBARI_RPM_FOLDER}
+mkdir -p ${OUTPUT_FOLDER} &&
+cp ambari/ambari-server/target/rpm/ambari-server/RPMS/x86_64/ambari-server-*.rpm ${OUTPUT_FOLDER}/ &&
 cp ambari/ambari-agent/target/rpm/ambari-agent/RPMS/x86_64/ambari-agent-*.rpm ${OUTPUT_FOLDER}/ &&
 cp ambari/ambari-metrics/ambari-metrics-assembly/target/rpm/ambari-metrics-assembly/RPMS/x86_64/ambari-metrics-assembly-*.rpm ${OUTPUT_FOLDER}/ &&
 cp ambari/ambari-metrics/ambari-metrics-assembly/target/rpm/ambari-metrics-collector/RPMS/x86_64/ambari-metrics-collector-*.rpm ${OUTPUT_FOLDER}/ &&
 cp ambari/ambari-metrics/ambari-metrics-assembly/target/rpm/ambari-metrics-grafana/RPMS/x86_64/ambari-metrics-grafana-*.rpm ${OUTPUT_FOLDER}/ &&
 cp ambari/ambari-metrics/ambari-metrics-assembly/target/rpm/ambari-metrics-hadoop-sink/RPMS/x86_64/ambari-metrics-hadoop-sink-*.rpm ${OUTPUT_FOLDER}/ &&
-cp ambari/ambari-metrics/ambari-metrics-assembly/target/rpm/ambari-metrics-monitor/RPMS/x86_64/ambari-metrics-monitor-*.rpm ${OUTPUT_FOLDER}/ 
+cp ambari/ambari-metrics/ambari-metrics-assembly/target/rpm/ambari-metrics-monitor/RPMS/x86_64/ambari-metrics-monitor-*.rpm ${OUTPUT_FOLDER}/ &&
 
 # Copy setup_repo.sh and generate the tarball
 cp phd-ci/setup_repo.sh ${OUTPUT_FOLDER} &&
 chown -R root:root ${OUTPUT_FOLDER} &&
 pushd ambari_artifacts &&
 createrepo ${AMBARI_RPM_FOLDER} &&
-tar cvzf ${AMBARI_RPM_FOLDER}.tar.gz ${AMBARI_RPM_FOLDER}
-popd &&
+tar cvzf ${AMBARI_RPM_FOLDER}.tar.gz ${AMBARI_RPM_FOLDER} &&
+popd
