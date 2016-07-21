ca7184624c683b4b990c695e99d0e20ece59f7c5
You shouldn't see this! <hawq-toolsmiths@pivotal.io>
2016-07-21 15:21:23 -0700

    updated

--- ambari-ci/jobs/build/scripts/build-ambari4.sh
+++ ambari-ci/jobs/build/scripts/build-ambari4.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-touch build-ambari/file1.txt
+set -x -e
+
+# Copy the generated rpms to target output folder
+mkdir -p ambari_artifacts
+OUTPUT_FOLDER=ambari_artifacts/${AMBARI_RPM_FOLDER}
+mkdir -p ${OUTPUT_FOLDER} &&
+
+cp phd-ci/setup_repo.sh ${OUTPUT_FOLDER} &&
+chown -R root:root ${OUTPUT_FOLDER} &&
+pushd ambari_artifacts &&
+createrepo ${AMBARI_RPM_FOLDER} &&
+tar cvzf ${AMBARI_RPM_FOLDER}.tar.gz ${AMBARI_RPM_FOLDER} &&
+popd
